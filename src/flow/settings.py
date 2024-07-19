65c4dbb7dc5892a60449da6a8d20ecdf44f4ff55
Nat1anWasTaken <nathan@nat1an.xyz>
2024-07-19 18:41:36 +0800

    feat(src/flow/settings.py): add character setting functionality and callback query handling

--- src/flow/settings.py
+++ src/flow/settings.py
@@ -1,6 +1,9 @@
 from pymongo.database import Database
 from telebot import TeleBot
-from telebot.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from telebot.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+from src.models.character import Character
+from src.models.user import User
 
 
 def generate_markup() -> InlineKeyboardMarkup:
@@ -15,7 +18,7 @@ def generate_markup() -> InlineKeyboardMarkup:
 
 class SettingsFlow:
     """
-    This isn't actually a flow, just some utility commands.
+    This isn't actually a flow, just a set of commands.
     """
 
     def __init__(self, bot: TeleBot, database: Database):
@@ -32,9 +35,29 @@ class SettingsFlow:
         # TODO: 回覆用戶一個幫助訊息，其中包含這個機器人的指令列表
         self.bot.reply_to(message, "Hello!")
 
+    def character(self, message: Message):
+        # TODO: /character 指令，用來設定用戶的角色
+        characters = Character.find(self.database)  # 可選的角色列表
+
+        # TODO: 回覆用戶訊息並讓用戶選擇角色，TIP: 使用 InlineKeyboardMarkup
+
+    def select_character(self, call: CallbackQuery):
+        character_id = ""  # TODO: 從 callback_data 中取得角色 ID
+
+        character = Character.find_one(self.database, character_id=character_id)
+        user_settings = User.find_one(self.database, user_id=call.from_user.id)
+        user_settings.selected_character_id = character_id
+        user_settings.upsert()
+
+        self.bot.send_message(call.message.chat.id, f"你選擇了: {character.prompt}")  # TODO: 改進這個訊息
+
 
 def setup(bot: TeleBot, database: Database):
     flow = SettingsFlow(bot, database)
 
     bot.register_message_handler(flow.start, commands=["start"])
     bot.register_message_handler(flow.help, commands=["help"])
+    bot.register_message_handler(flow.character, commands=["character"])
+    bot.register_callback_query_handler(
+        flow.select_character, lambda call: call.data in ["cb_yes", "cb_no"]
+    )  # TODO: 編輯這個 lambda 函數來過濾 callback_data
