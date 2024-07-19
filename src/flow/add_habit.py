625bb3f04c46f51875cde2ef23397fbaff781a62
Nat1anWasTaken <nathan@nat1an.xyz>
2024-07-19 18:49:12 +0800

    feat(src/flow/add_habit.py, src/utils.py): implement user settings check in AddHabitFlow; update ensure_user_settings function to return boolean value

--- src/flow/add_habit.py
+++ src/flow/add_habit.py
@@ -4,6 +4,7 @@ from telebot.types import Message
 
 from src.models.habit import Habit
 from src.ui.weekday_picker import Weekdays, WeekdayPicker
+from src.utils import ensure_user_settings
 
 
 class AddHabitFlow:
@@ -12,7 +13,10 @@ class AddHabitFlow:
         self.database: Database = database
 
     def add_habit(self, message: Message):
-        self.bot.reply_to(message, "What habit would you like to add?")
+        if not ensure_user_settings(self.bot, self.database, message):
+            return
+
+        self.bot.reply_to(message, "What habit would you like to add?")  # TODO: 編輯這個訊息
 
         self.bot.register_next_step_handler(message, self.habit_name)
 
@@ -21,8 +25,7 @@ class AddHabitFlow:
 
         self.bot.reply_to(
             message, f"Got it! You want to add the habit '{habit_name}'. What is the description of this habit?",
-        )
-        pass
+        )  # TODO: 編輯這個訊息
 
         picker = WeekdayPicker(self.bot, self.habit_weekdays, habit_name=habit_name)
         picker.start(message.chat.id)
@@ -30,7 +33,7 @@ class AddHabitFlow:
     def habit_weekdays(self, chat_id: int, weekdays: Weekdays, habit_name: str):
         message = self.bot.send_message(
             chat_id, "What time(s) would you like to do this habit? (e.g. 08:00, 12:00, 18:00)"
-        )
+        )  # TODO: 編輯這個訊息
 
         self.bot.register_next_step_handler(message, self.habit_times, habit_name=habit_name, weekdays=weekdays)
 
@@ -38,18 +41,19 @@ class AddHabitFlow:
         times = message.text.split(", ")
 
         if any(time not in ["{:02d}:00".format(i) for i in range(24)] for time in times):
-            self.bot.reply_to(message, "Invalid time format. Please use the format HH:00.")
+            self.bot.reply_to(message, "Invalid time format. Please use the format HH:00.")  # TODO: 編輯這個訊息
             return
 
         self.bot.reply_to(
             message, f"Got it! You want to add the habit '{habit_name}' on {weekdays} at {', '.join(times)}."
-        )
+        )  # TODO: 編輯這個訊息
 
         self.habit_upsert(message, habit_name=habit_name, weekdays=weekdays, times=times)
 
     def habit_upsert(self, message: Message, habit_name: str, weekdays: Weekdays, times: list[str]):
         habit = Habit(
             database=self.database,
+            owner_id=message.from_user.id,
             title=habit_name,
             habit_id="",
             weekdays=weekdays.flags,
@@ -58,11 +62,10 @@ class AddHabitFlow:
 
         habit.upsert()
 
-        self.bot.send_message(message.chat.id, "Habit added successfully!")
+        self.bot.send_message(message.chat.id, "Habit added successfully!")  # TODO: 編輯這個訊息
 
 
 def setup(bot: TeleBot, database: Database):
     flow = AddHabitFlow(bot, database)
 
     bot.register_message_handler(flow.add_habit, commands=["add_habit"])
-    # bot.register_callback_query_handler()
--- src/utils.py
+++ src/utils.py
@@ -5,10 +5,20 @@ from telebot.types import Message
 from src.models.user import User
 
 
-def ensure_user_settings(bot: TeleBot, database: Database, message: Message):
+def ensure_user_settings(bot: TeleBot, database: Database, message: Message) -> bool:
+    """
+    Ensure that the user has set up their settings.
+    :param bot: The bot instance.
+    :param database: The database instance.
+    :param message: The message instance.
+    :return: True if the user has set up their settings, False otherwise.
+    """
     user_settings = User.find_one(database, user_id=message.from_user.id)
 
-    if not user_settings:
-        bot.send_message(message.chat.id, "You haven't set up your settings yet. Please use the /start command.")  # TODO: 編輯這個訊息
-
+    if user_settings:
+        return True
+    else:
+        bot.send_message(
+            message.chat.id, "You haven't set up your settings yet. Please use the /start command."
+        )  # TODO: 編輯這個訊息
         return False
