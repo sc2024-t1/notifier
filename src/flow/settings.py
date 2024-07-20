638aedde4c43f9fec6a31f7219a4bbb00c700bde
Nat1anWasTaken <nathan@nat1an.xyz>
2024-07-20 21:18:20 +0800

    fix(src/flow/settings.py): update user name variable in welcome message

--- src/flow/settings.py
+++ src/flow/settings.py
@@ -27,19 +27,22 @@ class SettingsFlow:
     def __init__(self, bot: Notifier, database: Database):
         self.bot: Notifier = bot
         self.database: Database = database
-        
+
     def start(self, message: Message):
         # TODO: /start 指令
         # TODO: 回覆用戶一個歡迎訊息，其中包含這個機器人的基本使用方法，且讓用戶選擇他的初始角色，選擇角色的邏輯應該在下方的 character 方法中
         # TODO: (assigned to Ain)
         markup = InlineKeyboardMarkup()
         markup.row_width = 1
-        markup.add(InlineKeyboardButton("點我以開始選擇角色", callback_data= "start_character"))
-        self.bot.send_message(message.chat.id, f"{message.from_user.full_name} 好！歡迎使用本習慣養成軟體！你可以設定你的目標和時間，機器人會在適當的時候提醒你。\
-                              完成後，你可以與角色互動，系統會根據對話判斷你的完成狀態。本軟體也會定期統計你的習慣完成狀況，以熱力圖展示，還有各種成就等你解鎖喔～")
+        markup.add(InlineKeyboardButton("點我以開始選擇角色", callback_data="start_character"))
+        self.bot.send_message(
+            message.chat.id,
+            f"{message.from_user.full_name} 好！歡迎使用本習慣養成軟體！你可以設定你的目標和時間，機器人會在適當的時候提醒你。\n"
+            "完成後，你可以與角色互動，系統會根據對話判斷你的完成狀態。本軟體也會定期統計你的習慣完成狀況，以熱力圖展示，還有各種成就等你解鎖喔～"
+        )
         # TODO: call the function "character" by hitting the button
         return markup
-    
+
     def help(self, message: Message):
         # TODO: /help 指令
         # TODO: 回覆用戶一個幫助訊息，其中包含這個機器人的指令列表 ()
