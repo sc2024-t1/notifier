511760c4242bcb1f295c5f3077307ea2db0363e2
Nat1anWasTaken <nathan@nat1an.xyz>
2024-07-20 22:58:08 +0800

    feat(src/flow/settings.py): Add functionality to select and reset user characters, improve message prompts

--- src/flow/settings.py
+++ src/flow/settings.py
@@ -1,12 +1,13 @@
 from typing import Iterable
 
 from pymongo.database import Database
-from telebot.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from telebot.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 from src.bot import Notifier
 from src.database.character import Character
 from src.database.user import User
 from src.ui.character_picker import CharacterPicker
+from src.utils import ensure_user_settings
 
 
 def generate_markup() -> InlineKeyboardMarkup:
@@ -34,14 +35,30 @@ class SettingsFlow:
         # TODO: (assigned to Ain)
         markup = InlineKeyboardMarkup()
         markup.row_width = 1
-        markup.add(InlineKeyboardButton("點我以開始選擇角色", callback_data="start_character"))
+        markup.add(InlineKeyboardButton("點我以開始選擇角色", callback_data="select_character"))
+
         self.bot.send_message(
             message.chat.id,
             f"{message.from_user.full_name} 好！歡迎使用本習慣養成軟體！你可以設定你的目標和時間，機器人會在適當的時候提醒你。\n"
-            "完成後，你可以與角色互動，系統會根據對話判斷你的完成狀態。本軟體也會定期統計你的習慣完成狀況，以熱力圖展示，還有各種成就等你解鎖喔～"
+            "完成後，你可以與角色互動，系統會根據對話判斷你的完成狀態。本軟體也會定期統計你的習慣完成狀況，以熱力圖展示，還有各種成就等你解鎖喔～",
+            reply_markup=markup
         )
-        # TODO: call the function "character" by hitting the button
+
         return markup
+
+    def select_character(self, call: CallbackQuery):
+        self.bot.answer_callback_query(call.id, text="請開始選擇你的角色！")
+        characters: Iterable[Character] = Character.find(self.database)  # 可選的角色列表
+
+        picker = CharacterPicker(
+            bot=self.bot,
+            chat_id=call.message.chat.id,
+            callback=self.character_picking_callback,
+            characters=list(characters),
+            user_id=call.from_user.id
+        )
+
+        picker.start()
 
     def help(self, message: Message):
         # TODO: /help 指令
@@ -55,14 +72,14 @@ class SettingsFlow:
         picker = CharacterPicker(
             bot=self.bot,
             chat_id=message.chat.id,
-            callback=self.select_character,
+            callback=self.character_picking_callback,
             characters=list(characters),
             user_id=message.from_user.id
         )
 
         picker.start()
 
-    def select_character(self, chat_id: int, character: Character, user_id: int):
+    def character_picking_callback(self, chat_id: int, character: Character, user_id: int):
         self.bot.send_message(chat_id, text=f"✅ 成功將你的角色設定為 {character.name}！")
 
         character = Character.find_one(self.database, character_id=character.character_id)
@@ -77,6 +94,17 @@ class SettingsFlow:
         self.bot.send_chat_action(chat_id, action="typing")
         self.bot.send_message(chat_id, text=conversation.ask("你好"))
 
+    def reset(self, message: Message):
+        if not (user_settings := ensure_user_settings(self.bot, self.database, message)):
+            return
+
+        self.bot.conversation_manager.close_conversation(message.from_user.id)
+
+        self.bot.reply_to(
+            message,
+            text="成功重置你的對話！"
+        )
+
 
 def setup(bot: Notifier, database: Database):
     flow = SettingsFlow(bot, database)
@@ -84,6 +112,7 @@ def setup(bot: Notifier, database: Database):
     bot.register_message_handler(flow.start, commands=["start"])
     bot.register_message_handler(flow.help, commands=["help"])
     bot.register_message_handler(flow.character, commands=["character"])
+    bot.register_message_handler(flow.reset, commands=["reset"])
     bot.register_callback_query_handler(
-        flow.select_character, lambda call: call.data in ["cb_yes", "cb_no"]
-    )  # TODO: 編輯這個 lambda 函數來過濾 callback_data
+        flow.select_character, lambda call: call.data == "select_character"
+    )
